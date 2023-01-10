5cf54bf65458f5479418f938ae0dde5eaace044e
Lukas Szopo <me@szopo.com>
2023-01-10 03:05:42 -0800

    init commit

---
+++ CustomOutput.psm1
@@ -0,0 +1,41 @@
+#region documentation
+# writes messages to windows event log
+#endregion
+
+#region scriptheader
+#requires -version 5.0
+#endregion
+
+#region code
+class CustomOutput {
+    [CustomOutputType] $outputType
+
+    CustomOutput([string] $outputType) {
+        $this.outputType = [CustomOutputType]::new()
+        $this.outputType = $outputType
+    }
+    CustomOutput([CustomOutputType] $outputType) {
+        $this.outputType = $outputType
+    }
+
+    [void] WriteCustomOutput([string] $message) {
+        switch ($this.outputType) {
+            Console { Write-Host $message }
+            File { throw (New-Object System.NotImplementedException) }
+            EventLog { Write-CustomEventLog -message $message }
+            None { }
+        }
+    }
+
+    [void] WriteCustomEventLog([string] $message) {
+        Write-EventLog -LogName Application -Source "NAV PS Scripts" -EventId 1 -EntryType Information -Message $message
+    }
+}
+
+enum CustomOutputType {
+    Console
+    File
+    Event_Log
+    None
+}
+#endregion
